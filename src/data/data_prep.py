8c331e0680d46c438c813f199fd7f9bb930d8a02
Deepak Govindasamy <grc.deepak@gmail.com>
2024-03-09 14:51:51 -0800

    dev commit

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -4,4 +4,4 @@ import matplotlib.pyplot as plt
 
 df = pd.DataFrame({"A": [1, 2, 3], "B": [1, 2, 3]})
 
-df2 = pd.DataFrame({"A": [1, 2, 3], "B": [1, 2, 3]})
+df2 = pd.DataFrame({"C": [1, 2, 3], "D": [1, 2, 3]})
