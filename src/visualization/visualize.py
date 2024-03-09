2b5dab405622b46b4655be5b768b5e26b395371d
Deepak Govindasamy <grc.deepak@gmail.com>
2024-03-09 14:44:52 -0800

    first commit

---
+++ src/visualization/visualize.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+
+from sklearn.ensemble import RandomForestRegressor
+
+def create_list():
+    list_items = [
+        "Rice",
+        "Beans",
+        "Pulses",
+        "Rice",
+        "Beans",
+        "Pulses",
+        "Rice",
+        "Beans",
+        "Pulses",
+        "Rice",
+        "Beans",
+        "Pulses",
+    ]
+    print(list_items)
+
+
+create_list()
